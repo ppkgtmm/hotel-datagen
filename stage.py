034163f4c2691209c95174f7382d02e2162dbc01
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-02 02:19:59 +0700

    Update stage.py

--- stage.py
+++ stage.py
@@ -20,4 +20,4 @@ client = boto3.client(
 data_dir, bucket = getenv("SEED_DIR"), getenv("S3_BUCKET")
 for file_name in listdir(data_dir):
     file_path = path.join(data_dir, file_name)
-    client.upload_file(file_name, bucket, file_name)
+    client.upload_file(file_path, bucket, file_path)
